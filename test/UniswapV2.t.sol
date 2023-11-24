3024464e11d52cb4aae1a9065c0b99fa2680533e
pilnystudent <tomasludvik@seznam.cz>
2023-11-24 15:09:36 +0100

    .

--- test/UniswapV2.t.sol
+++ test/UniswapV2.t.sol
@@ -11,10 +11,10 @@ contract UniswapV2Test is Test {
         uniswapv2 = IUniswapV2(0x6ea7c26EC841C3936945dB349759df745f55ee60);
     }
 
-    function testAdd() public {
-        uniswapv2.addLiquidity(20620 * 1e18, 10 * 1e18);
-        uniswapv2.swap(10 * 1e18, true);
-        uniswapv2.swap(1 * 1e18, false);
+    function testAdd() public view {
+        //uniswapv2.addLiquidity(20620 * 1e18, 10 * 1e18);
+        //uniswapv2.swap(10 * 1e18, true);
+        //uniswapv2.swap(1 * 1e18, false);
         console2.log("Constant Product", uniswapv2.constantProduct());
         console2.log("Amount A", uniswapv2.amountA());
         console2.log("Amount B", uniswapv2.amountB());
